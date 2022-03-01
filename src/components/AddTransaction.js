72839229229d24f8a182fc477ef7e3fb7aa387a1
thuan <maiduc.thuan2512@gmail.com>
2022-03-01 23:41:55 +0700

    fix lan 3

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -5,6 +5,10 @@ export const AddTransaction = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
 
+  const onsetTextChange = (e) => {setText(e.target.value)}
+  const onsetAmountChange = (e) => 
+   {setAmount(e.target.value < 0 ? e.target.value.match('e')
+    (e.target.value = 0) : e.target.value)}
 
   return (
     <>
@@ -12,14 +16,25 @@ export const AddTransaction = () => {
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input className="input-layout" type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." maxLength="50" required/>
+          <input className="input-layout" 
+           type="text" 
+           value={text} 
+           onChange={onsetTextChange} 
+           placeholder="Enter text..." 
+           maxLength="50" 
+           required/>
         </div>
 
         <div className="form-control">
           <label htmlFor="amount">Amount <br />
             (negative - expense, positive - income)</label>
-          <input className="input-layout" type="number" value={amount} onChange={(e) => setAmount(e.target.value.match('[1-9]'))}
-             placeholder="Enter amount..." maxLength="10" required/>
+          <input className="input-layout" 
+           type="number" 
+           value={amount} 
+           onChange={onsetAmountChange}
+           placeholder="Enter amount..." 
+           maxLength="10" 
+           required/>
         </div>
         <button className="btn">Add transaction</button>
       </form>
