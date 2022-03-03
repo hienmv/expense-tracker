9b2f1c975a3ce22171c809447092be0c221cb389
thuan <maiduc.thuan2512@gmail.com>
2022-03-03 23:22:33 +0700

    fix lan 4

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,14 +1,29 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid'
 
 export const AddTransaction = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
 
-  const onsetTextChange = (e) => {setText(e.target.value)}
-  const onsetAmountChange = (e) => 
-   {setAmount(e.target.value < 0 ? e.target.value.match('e')
-    (e.target.value = 0) : e.target.value)}
+  const onSetTextChange = (e) => {
+    setText(e.target.value);
+  };
+  const onSetAmountChange = (e) => {
+    setAmount(
+      e.target.value < 0
+        ? (e.target.value = 0)
+        : e.target.value
+    );
+    
+    e.target.value = 'e'
+        ? (e.target.value = 'error')
+        : e.target.value
+  };
+
+  const onAddBtnClick = (e) => {
+    setText([...text], {id: 'v4', name: text});
+    setAmount([...amount], {id: 'v4', name: amount});
+  }
 
   return (
     <>
@@ -16,28 +31,34 @@ export const AddTransaction = () => {
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input className="input-layout" 
-           type="text" 
-           value={text} 
-           onChange={onsetTextChange} 
-           placeholder="Enter text..." 
-           maxLength="50" 
-           required/>
+          <input
+            className="input-layout"
+            type="text"
+            value={text}
+            onChange={onSetTextChange}
+            placeholder="Enter text..."
+            maxLength="50"
+            required
+          />
         </div>
 
         <div className="form-control">
-          <label htmlFor="amount">Amount <br />
-            (negative - expense, positive - income)</label>
-          <input className="input-layout" 
-           type="number" 
-           value={amount} 
-           onChange={onsetAmountChange}
-           placeholder="Enter amount..." 
-           maxLength="10" 
-           required/>
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            className="input-layout"
+            type="number"
+            value={amount}
+            onChange={onSetAmountChange}
+            placeholder="Enter amount..."
+            maxLength="10"
+            required
+          />
         </div>
-        <button className="btn">Add transaction</button>
+        <button className="btn" onClick={onAddBtnClick}>Add transaction</button>
       </form>
     </>
-  )
-}
+  );
+};
