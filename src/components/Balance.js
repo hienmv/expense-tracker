bbeaa44c2fb59152bf05b4ed2c332d4201ff8a38
hienmv <hienmv.vn@gmail.com>
2022-03-27 16:58:40 +0900

    refactor code: reduce hook invocations

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -6,31 +6,30 @@ export const Balance = (updatedFlag) => {
   const [revenue, setRevenue] = useState(0);
 
   React.useEffect(() => {
-    const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+    const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
     if (transactions.length > 0) {
       let _cost = 0;
-      let _revenue = 0; 
-      transactions.forEach(transaction => {
+      let _revenue = 0;
+      transactions.forEach((transaction) => {
         const amount = parseFloat(transaction.amount);
         if (amount < 0.0) {
           _cost += amount;
         } else {
           _revenue += amount;
         }
-        setCost(_cost * (-1));
-        setRevenue(_revenue);
-        setBalance(_cost + _revenue);
       });
+      setCost(_cost * -1);
+      setRevenue(_revenue);
+      setBalance(_cost + _revenue);
     }
   }, [updatedFlag]);
 
-
   return (
     <>
-    <h3>Summary</h3>
+      <h3>Summary</h3>
       <h4>Balance: {balance}</h4>
       <h5>Cost: {cost}</h5>
       <h5>Revenue: {revenue}</h5>
     </>
-  )
+  );
 };
