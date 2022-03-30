c3cd3b9ae487619d185dcabaf8af2fdf7f4f4048
MDHUNG <92449546+MDHUNG@users.noreply.github.com>
2022-03-30 14:21:29 +0700

    Revert "add task 2"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { Header } from './components/Header';
-
-import { TransactionList } from './components/TransactionList';
-
 import { AddTransaction } from './components/AddTransaction';
-
-
-
 import './App.css';
 
 function App() {
@@ -14,7 +8,6 @@ function App() {
     <div>
       <Header />
       <div className="container">
-        <TransactionList/>
         <AddTransaction />
       </div>
     </div>
