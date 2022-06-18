42f39e1d23218a6a50ec53475b199d86eef4f567
Healer <hienthulinhnhi@gmail.com>
2022-06-18 18:44:47 +0700

    tuananhne

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from './components/Header';
 import { AddTransaction } from './components/AddTransaction';
 import './App.css';
+import { TransactionHistory } from './components/TransactionHistory';
+import { Balance } from './components/Balance';
 
 function App() {
+  const [updatedFlag, setUpdatedFlag] = useState(false);
+  const toggleUpdatedFlag = () => {
+    setUpdatedFlag(updatedFlag => !updatedFlag);
+  }
+
   return (
     <div>
       <Header />
       <div className="container">
-        <AddTransaction />
+        <Balance updatedFlag={updatedFlag} />
+        <TransactionHistory updatedFlag={updatedFlag} />
+        <AddTransaction toggleUpdatedFlag={toggleUpdatedFlag} />
       </div>
     </div>
   );
